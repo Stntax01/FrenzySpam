9836b83991f8579acf9bbf6f344c5976dac2d173
Jaadugar <126132553+Stntax01@users.noreply.github.com>
2023-02-28 15:27:25 +0530

    Update start.py

--- Frenzy/modules/start.py
+++ Frenzy/modules/start.py
@@ -8,11 +8,11 @@ PythonButton = [
         Button.inline("• ᴄᴏᴍᴍᴀɴᴅs •", data="help_back")
         ],
         [
-        Button.url("• ᴄʜᴀɴɴᴇʟ •", "https://t.me/TheAltron"),
-        Button.url("• sᴜᴘᴘᴏʀᴛ •", "https://t.me/AltronChats")
+        Button.url("• ᴄʜᴀɴɴᴇʟ •", "https://t.me/FrenzyBots"),
+        Button.url("• sᴜᴘᴘᴏʀᴛ •", "https://t.me/FrenzyChats")
         ],
         [
-        Button.url("• ʀᴇᴘᴏ •", "https://github.com/ItZxSTaR/TheBotSpam")
+        Button.url("• ʀᴇᴘᴏ •", "https://t.me/FrenzyBots")
         ]
         ]
 
