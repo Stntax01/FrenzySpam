2dafbd2296325071a5a17bb70f3d6f8e5cc4011f
Jaadugar <126132553+Stntax01@users.noreply.github.com>
2023-02-28 20:40:14 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -12,8 +12,8 @@ def load_plugins(plugin_name):
     load = importlib.util.module_from_spec(spec)
     load.logger = logging.getLogger(plugin_name)
     spec.loader.exec_module(load)
-    sys.modules["AltronX.modules." + plugin_name] = load
-    print("Altron has Imported " + plugin_name)
+    sys.modules["Frenzy.modules." + plugin_name] = load
+    print("Frenzy has Imported " + plugin_name)
 
 async def edit_or_reply(event, text):
     if event.sender_id in SUDO_USERS:
